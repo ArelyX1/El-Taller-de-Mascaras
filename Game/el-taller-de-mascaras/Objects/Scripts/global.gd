4a433664ba60425a66b64975364aa60e2c8d3c34
ArelyX1 <andrycaceres2006@gmail.com>
2026-01-28 07:33:45 -0500

    Movimiento y dialogos

---
+++ Game/el-taller-de-mascaras/Objects/Scripts/global.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func DialogicStart(dialogicPath):
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	Dialogic.start(dialogicPath)
+	Dialogic.timeline_ended.connect(func(): Input.mouse_mode = Input.MOUSE_MODE_CAPTURED)
+ 	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
